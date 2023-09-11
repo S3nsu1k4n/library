92cc68dd451540e20a0f4fcbaef72424aede1d10
Sascha Koeppe <saschakoeppe@gmail.com>
2023-09-11 18:01:58 +0900

    add dialog

--- library.js
+++ library.js
@@ -1,5 +1,24 @@
 #!/usr/bin/node
 
+output_section = document.querySelector(".output-section");
+input_section = document.querySelector(".input-section");
+dialog = document.querySelector("dialog");
+dialog_button = document.querySelector(".dialog-button");
+new_book_button = document.querySelector(".new-book-button");
+
+new_book_button.addEventListener("click", () => {
+  dialog.showModal();
+})
+
+dialog_button.addEventListener("click", () => {
+  
+  output_section.innerHTML = '';
+  get_books();
+  dialog.close();
+})
+
+const myLibrary = [];
+
 class Book {
   constructor(title, author, no_pages, already_read) {
     this.title = title
@@ -21,11 +40,39 @@ class Book {
   }
 }
 
+function addBookToLibrary(book) {
+  // take users input
+  // store the new book object in array
 
-const book1 = new Book('some title', 'famous author', 254, false)
-const book2 = new Book('some title2', 'famous author2', 234, true)
-console.log(book1.info())
-console.log(book1.type())
-console.log(book2.valueOf())
+  myLibrary.push(book);
+}
 
-console.log(Object.getPrototypeOf(book1) == Book.prototype)
+function get_books() {
+  myLibrary.forEach(function (book) {
+    // display content on page
+    const card = document.createElement('div');
+    card.classList.add("card")
+    for (const k of Object.keys(book.info())){
+      const p = document.createElement('p');
+      p.textContent = `${k}: ${book[k]}`;
+      card.appendChild(p);
+    }
+
+    output_section.appendChild(card);
+    console.log(book)
+  });
+}
+
+
+const book1 = new Book('some title', 'famous author', 254, false);
+const book2 = new Book('some title2', 'famous author2', 234, true);
+console.log(book1.info());
+console.log(book1.type());
+console.log(book2.valueOf());
+
+console.log(Object.getPrototypeOf(book1) == Book.prototype);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+get_books();
